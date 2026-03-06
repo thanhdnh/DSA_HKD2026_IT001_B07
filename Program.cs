8922102b84c48c11dad0993e3958377e9da796f3
thanhdnh <myhoangthanh@yahoo.com>
2026-03-06 14:38:38 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -31,15 +31,65 @@ public class PhoneList:DictionaryBase
         return false;
     }
 }
+
+public class BucketHash{
+   private const int SIZE = 10;
+   ArrayList[] data;
+   public BucketHash(){
+      data = new ArrayList[SIZE];
+      for(int i=0; i<=SIZE-1; i++)
+         data[i] = new ArrayList(4);
+   }
+   public int Hash(string s){//hash function
+      long tot = 0;
+      char[] chararray;
+      chararray = s.ToCharArray();
+      for(int i=0; i<=s.Length-1; i++)
+         tot+=37*tot+(int)chararray[i];
+      tot=tot%data.GetUpperBound(0);
+      if(tot<0)
+         tot+=data.GetUpperBound(0);
+      return (int)tot;
+   }
+   public void Insert(string item){
+      int hash_value = Hash(item);
+      if(!data[hash_value].Contains(item))
+         data[hash_value].Add(item);
+   }
+   public void Remove(string item){
+      int hash_value = Hash(item);
+      if(data[hash_value].Contains(item))
+         data[hash_value].Remove(item);
+   }
+}
 public class Program
 {
   public static void Main(string[] args)
   {
+    /*
     PhoneList contact = new PhoneList();
     contact.Add("Nam", "0123001");
     contact.Add("Hoa", "0123002");
     contact.Add("Xuan", "0123003");
     Console.WriteLine($"User Nam: {contact.SearchByUser("Hoa")}");
     Console.WriteLine($"Phone 0123002: {contact.SearchByPhoneNumber("0123002")}");
+    */
+
+    string[] keys = {
+                        "Nam",
+                        "Bình",
+                        "Minh",
+                        "Xuân",
+                        "Hạ",
+                        "Thu",
+                        "Đông",
+                        "Mai",
+                        "Đào",
+                        "Bích",
+                        "Lê",
+                        "Mận"
+                    };
+    for(int i=0; i<keys.Length; i++)
+        Console.WriteLine(keys[i] + " => " + (new BucketHash()).Hash(keys[i]));
   }
 }
--- obj/Debug/net9.0/B07.AssemblyInfo.cs
+++ obj/Debug/net9.0/B07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd5f5bb410d88ba50de25ed6d18b6a34c8817b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("B07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
